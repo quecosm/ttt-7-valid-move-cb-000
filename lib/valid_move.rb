ee669d2f0ace47ea840eb69b74848d3896d237e1
Queco SM <sersalazar.marioni@gmail.com>
2018-07-23 14:57:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def valid_move? (board, index)
 
   def position_taken?(array, ind)
-    index = index.to_i - 1
+    ind = index.to_i - 1
     taken = nil
     if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     taken = false

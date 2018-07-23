8e3b0bbe628b8edc34a1ecac7207c8183ce3948c
Queco SM <sersalazar.marioni@gmail.com>
2018-07-23 15:16:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move? (board, index)
-  ind = index.to_i - 1
+  
   def position_taken?(array, ind)
         
     if (board[ind] ==  " " || board[ind] == "" || board[ind] == nil)
